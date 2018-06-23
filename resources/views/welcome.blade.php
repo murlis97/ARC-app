3a9a67862f1a459a2d215b2ffeb359b8db527cf8
Unknown <murlidharsharma69@gmail.com>
2018-06-23 15:47:46 +0530

    Admin template

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Dashboard App
                 </div>
 
                 <div class="links">
